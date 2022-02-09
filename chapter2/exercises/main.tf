27494a574b9106517bdd9a0c1b377eb4dab9df2f
munnep <patrick.munne@hashicorp.com>
2022-02-09 12:28:43 +0100

    exercise single webserver

---
+++ chapter2/exercises/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-074251216af698218"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data       = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF                 
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  min_size             = 2
+  max_size             = 10
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+
+
+
+resource "aws_security_group" "instance" {
+  name = "patrick-terraform-example-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "the public ip address of the web server"
+}
