3a63382de4f33faae3d042807b63bc11e92f51d2
munnep <patrick.munne@hashicorp.com>
2022-02-09 12:36:17 +0100

    with autoscaling no loadbalancer

--- chapter2/exercises/main.tf
+++ chapter2/exercises/main.tf
@@ -2,11 +2,21 @@ provider "aws" {
   region = "us-west-2"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
 }
+
 
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-074251216af698218"
@@ -16,11 +26,18 @@ resource "aws_launch_configuration" "example" {
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
-              EOF                 
+              EOF 
+
+  # Required when using a launch configuration with an auto scaling group. 
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
   min_size             = 2
   max_size             = 10
   tag {
